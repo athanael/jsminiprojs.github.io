7fcb7cb3563ccae5795bb8c3fce8a54372fdfc4c
athanael <pilotewpa@gmail.com>
2021-07-04 10:23:14 +0200

    ok

---
+++ api-meteo/index.js
@@ -0,0 +1,28 @@
+document.querySelector("#button").addEventListener("click", function (e) {
+  e.preventDefault();
+  const APIKEY = "138bbb8f6daeaf81f8bec423a6e308b5";
+  let ville = document.querySelector("#ville").value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${APIKEY}&units=metric&lang=fr`;
+  fetch(url)
+    .then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        if (data.cod == 400 || data.cod == 404) {
+          alert("Cette ville n'existe pas");
+        } else {
+          document.getElementById("city").innerText = ville;
+          document.getElementById("temperature").innerText =
+            parseInt(data.main.temp) + "°C";
+          document.getElementById("weather").innerText =
+            data.weather[0].description;
+          document.getElementById("humidity").innerText =
+            data.main.humidity + "%";
+          let icons = document.getElementsByTagName("i");
+          for (let icon of icons) {
+            icon.classList.remove("hidden");
+          }
+        }
+      })
+    )
+    .catch((err) => console.log("erreur : " + err));
+});
